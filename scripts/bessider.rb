80060515731c815907be7c64b6c7e1b5d474754d
Mike Allred <mikejallred@gmail.com>
2016-06-10 00:45:30 +0000

    Rebuilding, cobbling together.

---
+++ scripts/bessider.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# purpose: run besside-ng on a wlan interface
+# relies on the aircrack-ng package
+
+if_wlan = 'wlan3'
+if_mon = 'mon0'
+dir_output = '/home/pi/besside'
+Dir.mkdir dir_output unless Dir.exists? dir_output
+Dir.chdir dir_output
+cmd_airmon_start =<<-eoc
+  nohup sudo bash -c "ifdown wlan0; ifdown wlan1; ifdown wlan2; airmon-ng start #{if_wlan}; ifup wlan0; ifup wlan1; ifup wlan2"
+eoc
+  #nohup sudo bash -c "ifdown wlan0; ifdown wlan2; airmon-ng start #{if_wlan}; ifup wlan0; ifup wlan2"
+system cmd_airmon_start
+
+cmd_besside =<<-eoc
+  sudo /usr/sbin/besside-ng #{if_mon}
+eoc
+system cmd_besside
